06ac7af4df1444f65041c1288ea8b0d027321099
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-10-24 14:41:30 +0200

    FE - user - Hook done, works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
-import user from "./model/User";
 import User from "./model/User";
+import useUsers from "./hooks/useUsers";
 
 function App() {
 
-  const [message, setMessage] = useState("");
-  const [users, setUsers] = useState([]);
+  const {users} = useUsers();
 
 
-  useEffect(()=>{
-    fetchMessage()
-  },[])
-
-  useEffect(()=>{
-    fetchUser()
-  },[])
-
-  function fetchUser(){
-    axios.get("/api/user")
-        .then(response => response.data)
-        .then(users => setUsers(users))
-        .catch((error) => console.log(error))
-
-  }
-
-
-  function fetchMessage(){
-    axios.get("/api/hello")
-          .then(response => response.data)
-        .then(data => setMessage(data))
-        .catch((error) => console.log(error))
-
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>hallo ?!?</p>
-
         {users.map((user : User)=>{
           return <p>{user.companyName}</p>
         })}

c6d96b58dbb56cec5578c4915c67cf490e59d75b
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-10-24 14:27:53 +0200

    FE - Fetch from FE works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,30 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import user from "./model/User";
+import User from "./model/User";
 
 function App() {
 
   const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
 
 
   useEffect(()=>{
     fetchMessage()
   },[])
 
+  useEffect(()=>{
+    fetchUser()
+  },[])
+
+  function fetchUser(){
+    axios.get("/api/user")
+        .then(response => response.data)
+        .then(users => setUsers(users))
+        .catch((error) => console.log(error))
+
+  }
 
 
   function fetchMessage(){
@@ -26,6 +40,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
+        <p>hallo ?!?</p>
+
+        {users.map((user : User)=>{
+          return <p>{user.companyName}</p>
+        })}
       </header>
     </div>
   );

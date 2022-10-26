2a2976ac3147b99d50bb31da8294832693cff034
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-10-26 11:46:05 +0200

    BE - EvaluationService.java added

---
+++ backend/src/main/java/de/neuefische/backend/service/EvaluationService.java
@@ -0,0 +1,46 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.model.Evaluation;
+import de.neuefische.backend.repository.EvaluationRepo;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class EvaluationService {
+
+    private final EvaluationRepo evaluationRepo;
+
+    private final IdService idService;
+
+    @Autowired
+    public EvaluationService(EvaluationRepo evaluationRepo, IdService idService) {
+        this.evaluationRepo = evaluationRepo;
+        this.idService = idService;
+    }
+
+    public List<Evaluation> getAllEvaluations(){
+        return evaluationRepo.findAll();
+    }
+
+    public Evaluation addEvaluation(@NotNull Evaluation eva){
+        eva.setId(idService.generateID());
+        return evaluationRepo.save(eva);
+    }
+
+    public void deleteEvaluation(String id){
+        Optional<Evaluation> evaluationOptional = evaluationRepo.findById(id);
+
+        if(evaluationOptional.isPresent()){
+            Evaluation foundEvaluation = evaluationOptional.get();
+            evaluationRepo.delete(foundEvaluation);
+        }else {
+            throw new NoSuchElementException("Gef¨¨rdungsbeurteilung nicht gefunden");
+        }
+
+    }
+}

44f1cae92977eb9d33e34f00f59d2d9c90f95575
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-11-15 17:15:09 +0100

    FE - UserCreateNewEva2.tsx done and ready for Backend !!!

--- frontend/src/pages/userCreateNewEva/UserCreateNewEva2.tsx
+++ frontend/src/pages/userCreateNewEva/UserCreateNewEva2.tsx
@@ -2,7 +2,7 @@ import User from "../../model/User";
 import {NavLink} from "react-router-dom";
 import Evaluation from "../../model/Evaluation";
 import "./UserCreateNewEva2Styled.css";
-import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import {ChangeEvent, FormEvent, SyntheticEvent, useEffect, useState} from "react";
 import axios from "axios";
 import UsersEva from "../../model/UsersEva";
 
@@ -11,77 +11,123 @@ type UserCreateNewEva2Props ={
     catalog : Map<number,Evaluation[]>;
 }
 
-export default function UserCreateNewEva2(props: UserCreateNewEva2Props){
+export default function UserCreateNewEva2(props: UserCreateNewEva2Props) {
 
-    const [qList, setQList] = useState(Array<Evaluation>)
-
-    useEffect(()=>{
-
-       const eveArray = Array.from(props.catalog, ([name, value]) => ( value ));
-        const flattenedEvas = eveArray.flatMap(eva => eva);
-        setQList(flattenedEvas);
-        setUserEva((prevUserEva)=>{return{...prevUserEva, evaCatalog:flattenedEvas}})
-    },[])
-
-    const emptyUserEva : UsersEva = {
-        mail : props.thisUser.mail,
-        accountName : props.thisUser.accountName,
+    const emptyUserEva: UsersEva = {
+        mail: props.thisUser.mail,
+        accountName: props.thisUser.accountName,
         manageFirstName: props.thisUser.manageFirstName,
-        manageLastName : props.thisUser.manageLastName,
-        companyName : props.thisUser.companyName,
-        companyStreet : props.thisUser.companyStreet,
-        companyZip : props.thisUser.companyZip,
-        companyLocation : props.thisUser.companyLocation,
+        manageLastName: props.thisUser.manageLastName,
+        companyName: props.thisUser.companyName,
+        companyStreet: props.thisUser.companyStreet,
+        companyZip: props.thisUser.companyZip,
+        companyLocation: props.thisUser.companyLocation,
         employee1: props.thisUser.employee1,
         employee2: props.thisUser.employee2,
         employee3: props.thisUser.employee3,
         employee4: props.thisUser.employee4,
         employee5: props.thisUser.employee5,
-        medicalCareName : props.thisUser.medicalCareName,
-        medicalCareStreet : props.thisUser.medicalCareStreet,
-        medicalCareZip : props.thisUser.medicalCareZip,
-        medicalCareLocation : props.thisUser.medicalCareLocation,
+        medicalCareName: props.thisUser.medicalCareName,
+        medicalCareStreet: props.thisUser.medicalCareStreet,
+        medicalCareZip: props.thisUser.medicalCareZip,
+        medicalCareLocation: props.thisUser.medicalCareLocation,
         evaName: "",
-        forWhom:"",
-        forWhomStreet:"",
-        forWhomZip:"",
+        forWhom: "",
+        forWhomStreet: "",
+        forWhomZip: "",
         forWhomLocation: "",
         evaCatalog: []
     }
 
-    const [userEva, setUserEva] = useState(emptyUserEva);
+    const [userEva, setUserEva] = useState<UsersEva>(emptyUserEva);
 
+    const [qList, setQList] = useState(Array<Evaluation>)
 
-    function handleChange(event : ChangeEvent<HTMLInputElement>){
-
-
-        const name = event.target.name;
-        const newValue = event.target.value;
-
-       setUserEva((prevUsersEva) => ({...prevUsersEva,
-            [name]: event.target.type === "checkbox" ? event.target.checked : newValue
-       }))
+    function findOutIndexOfEva(key: string) {
+        for (let i = 0; i < qList.length; i++) {
+            const categoryKey = userEva.evaCatalog[i].category + userEva.evaCatalog[i].inCategoryNum;
+            if (categoryKey === key) {
+                return i;
+            }
+        }
+        return 0;
     }
 
-    function handleCatalogChange(event : ChangeEvent<HTMLInputElement>,id:string){
-
-
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
         const name = event.target.name;
         const newValue = event.target.value;
 
-        setUserEva((prevUsersEva) => ({...prevUsersEva,
+        setUserEva((prevUsersEva) => ({
+            ...prevUsersEva,
             [name]: event.target.type === "checkbox" ? event.target.checked : newValue
         }))
     }
 
-    const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+    function handleCatalogChange( key: string,event: ChangeEvent<HTMLInputElement>) {
+
+        const name = event.target.name;
+        const value = event.target.value;
+
+
+        setUserEva((prevUserEva) => {
+
+            const evaIndexToUpdate: number = findOutIndexOfEva(key);
+
+
+            const updatedEva = {
+                ...prevUserEva.evaCatalog[evaIndexToUpdate],
+                [name]: event.target.type === "checkbox" ? event.target.checked : value
+            };
+
+
+            prevUserEva.evaCatalog[evaIndexToUpdate] = updatedEva;
+
+            return { ...prevUserEva };
+        });
+
+    }
+
+    function handleSelectChange( key: string,event: SyntheticEvent<HTMLSelectElement>) {
+
+        const name = event.currentTarget.name;
+        const value = event.currentTarget.value;
+
+
+        setUserEva((prevUserEva) => {
+
+            const evaIndexToUpdate: number = findOutIndexOfEva(key);
+
+
+            const updatedEva = {
+                ...prevUserEva.evaCatalog[evaIndexToUpdate],
+                [name]: value
+            };
+
+
+            prevUserEva.evaCatalog[evaIndexToUpdate] = updatedEva;
+
+
+            return { ...prevUserEva };
+        });
+
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log(userEva)
     }
 
+    useEffect(() => {
 
+        const eveArray = Array.from(props.catalog, ([name, value]) => (value));
+        const flattenedEvas = eveArray.flatMap(eva => eva);
+        setQList(flattenedEvas);
+        setUserEva((prevUserEva) => {
+            return {...prevUserEva, evaCatalog: flattenedEvas}
+        })
+    }, [])
 
-    return(
+    return (
         <main>
 
             <form className={"forWhomForm"} onSubmit={handleSubmit}>
@@ -93,66 +139,77 @@ export default function UserCreateNewEva2(props: UserCreateNewEva2Props){
                     placeholder={`Beurteilungsname`}
                 />
                 <h2>Gefährdungsbeurteilung für:</h2>
-                        <label htmlFor={"forWhom"}>Name:</label>
-                        <input
-                            type={"text"}
-                            name={"forWhom"}
-                            onChange={handleChange}
-                            placeholder={`${props.thisUser.companyName}`}
-                        />
-                        <label htmlFor={"forWhomStreet"}>Straße Hausnummer:</label>
-                        <input
-                            type={"text"}
-                            name={"forWhomStreet"}
-                            onChange={handleChange}
-                            placeholder={`${props.thisUser.companyStreet}`}
-                        />
-                        <label htmlFor={"forWhomZip"}>Postleitzahl:</label>
-                        <input
-                            type={"text"}
-                            name={"forWhomZip"}
-                            onChange={handleChange}
-                            placeholder={`${props.thisUser.companyZip}`}
-                        />
-                        <label htmlFor={"forWhomLocation"}>Ort:</label>
+                <label htmlFor={"forWhom"}>Name:</label>
+                <input
+                    type={"text"}
+                    name={"forWhom"}
+                    onChange={handleChange}
+                    placeholder={`${props.thisUser.companyName}`}
+                />
+                <label htmlFor={"forWhomStreet"}>Straße Hausnummer:</label>
+                <input
+                    type={"text"}
+                    name={"forWhomStreet"}
+                    onChange={handleChange}
+                    placeholder={`${props.thisUser.companyStreet}`}
+                />
+                <label htmlFor={"forWhomZip"}>Postleitzahl:</label>
+                <input
+                    type={"text"}
+                    name={"forWhomZip"}
+                    onChange={handleChange}
+                    placeholder={`${props.thisUser.companyZip}`}
+                />
+                <label htmlFor={"forWhomLocation"}>Ort:</label>
 
-                        <input
-                            type={"text"}
-                            name={"forWhomLocation"}
-                            onChange={handleChange}
-                            placeholder={`${props.thisUser.companyLocation}`}
-                        />
+                <input
+                    type={"text"}
+                    name={"forWhomLocation"}
+                    onChange={handleChange}
+                    placeholder={`${props.thisUser.companyLocation}`}
+                />
 
+                {qList.map((e: Evaluation) => {
 
-            {qList.map((e:Evaluation,ident)=>{
+                    return (
+                        <article key={e.category + e.inCategoryNum} className={"question"} >
 
-                return(
-                    <article id={e.category+e.id} className={"question"}>
+                            <p className={"text"}>{e.txtBlock}</p>
+                            {e.subTxt && <p className={"subTXT"}>{e.subTxt}</p>}
+                            <ul>
+                                {e.subListItem1 && <li>{e.subListItem1}</li>}
+                                {e.subListItem2 && <li>{e.subListItem2}</li>}
+                                {e.subListItem3 && <li>{e.subListItem3}</li>}
+                                {e.subListItem4 && <li>{e.subListItem4}</li>}
+                                {e.subListItem5 && <li>{e.subListItem5}</li>}
+                            </ul>
 
-                        <p className={"text"}>{e.txtBlock}</p>
-                        {e.subTxt && <p className={"subTXT"}>{e.subTxt}</p>}
-                        <ul>
-                            {e.subListItem1 && <li>{e.subListItem1}</li>}
-                            {e.subListItem2 && <li>{e.subListItem2}</li>}
-                            {e.subListItem3 && <li>{e.subListItem3}</li>}
-                            {e.subListItem4 && <li>{e.subListItem4}</li>}
-                            {e.subListItem5 && <li>{e.subListItem5}</li>}
-                        </ul>
+                            <div className={"userInteraction"} key={e.category+e.inCategoryNum}>
 
-                        <div className={"userInteraction"}>
+                                <label className={"labelValuePair"} htmlFor={"done"}>Erledigt ?
+                                    <input className={"dataInput"}  name="done"
+                                           checked={e.done}
+                                           onChange={(event)=>
+                                           {handleCatalogChange(e.category+e.inCategoryNum,event)}
+                                           }
+                                           type={"checkbox"}/></label>
 
-                        <label className={"labelValuePair"} htmlFor={"done"} >Wurden Die Maßnahmen umgesetzt ?
-                            <input className={"checker"} onChange={handleChange} name="done" type={"checkbox"} checked={e.done}/></label>
-
-                        <p>Sind Maßnahmen erforderlich ? Wenn ja:</p>
+                                <p>Sind Maßnahmen erforderlich ? Wenn ja:</p>
 
                                 <label className={"labelValuePair"} htmlFor={"doneTil"}>Bis wann ?
-                                    <input className={"dataInput"} onChange={handleChange} name="doneTil"  type={"date"}/></label>
-
+                                    <input className={"dataInput"}  name="doneTil"
+                                           value={e.doneTil}
+                                           onChange={(event)=>
+                                               {handleCatalogChange(e.category+e.inCategoryNum,event)}
+                                           }
+                                           type={"date"}/></label>
 
                                 <label className={"labelValuePair"} htmlFor={"who"}>Von wem ?
-                                    <select name="respPerson" onSelect={()=>handleChange} className={"dataInput"} >
-                                        <option value={props.thisUser.manageLastName}>{props.thisUser.manageLastName}</option>
+                                    <select name="respPerson"
+                                            value={e.respPerson}
+                                            onChange={(event) => {handleSelectChange(e.category+e.inCategoryNum,event)}} className={"dataInput"}>
+                                        <option
+                                            value={props.thisUser.manageLastName}>{props.thisUser.manageLastName}</option>
                                         <option value={props.thisUser.employee1}>{props.thisUser.employee1}</option>
                                         <option value={props.thisUser.employee2}>{props.thisUser.employee2}</option>
                                         <option value={props.thisUser.employee3}>{props.thisUser.employee3}</option>
@@ -161,18 +218,39 @@ export default function UserCreateNewEva2(props: UserCreateNewEva2Props){
 
                                     </select></label>
 
-                            <label className={"labelValuePair"} htmlFor={"isDone"} >Maßnahmen erledigt ?
-                                <input className={"checker"} onChange={handleChange} name="isDone" type={"checkbox"} checked={e.done}/></label>
+                                <label className={"labelValuePair"} htmlFor={"who"}>Kontrolle durch:
+                                    <select name="controlDone"
+                                            value={e.controlDone}
+                                            onChange={(event) => {handleSelectChange(e.category+e.inCategoryNum,event)}} className={"dataInput"}>
+                                        <option
+                                            value={props.thisUser.manageLastName}>{props.thisUser.manageLastName}</option>
+                                        <option value={props.thisUser.employee1}>{props.thisUser.employee1}</option>
+                                        <option value={props.thisUser.employee2}>{props.thisUser.employee2}</option>
+                                        <option value={props.thisUser.employee3}>{props.thisUser.employee3}</option>
+                                        <option value={props.thisUser.employee4}>{props.thisUser.employee4}</option>
+                                        <option value={props.thisUser.employee5}>{props.thisUser.employee5}</option>
 
-                        </div>
+                                    </select></label>
 
-                    </article>
-                )
-            })}
-                <input type={"submit"}/>
-                 <NavLink to={"/userCreateNewEva"}>Zurück...</NavLink>
-        </form>
+                                <label className={"labelValuePair"} htmlFor={"control"}>Erledigt ?
+                                    <input className={"dataInput"}  name="control"
+                                           checked={e.control}
+                                           onChange={(event)=>
+                                           {handleCatalogChange(e.category+e.inCategoryNum,event)}
+                                           }
+                                           type={"checkbox"}/></label>
+
+                            </div>
+
+                        </article>
+                    )
+
+                })}
+
+                <NavLink to={"/userCreateNewEva"}>Zurück...</NavLink>
+                <input type="submit"/>
+            </form>
 
         </main>
-)
+    )
 }

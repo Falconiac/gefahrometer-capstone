4ebc59e74a73fbd4b61b7cbfc698f2bbc98fd0e2
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-11-17 15:49:49 +0100

    FE- CRUD done

--- frontend/src/pages/evaUserArea/EvaUserArea.tsx
+++ frontend/src/pages/evaUserArea/EvaUserArea.tsx
@@ -1,5 +1,5 @@
 import User from "../../model/User";
-import {useEffect, useState} from "react";
+import { useState} from "react";
 import useUsers from "../../hooks/useUsers";
 import axios from "axios";
 import UsersEva from "../../model/UsersEva";
@@ -16,9 +16,6 @@ export default function EvaUserArea(props: EvaUserProps){
     const [userEvas, setUserEvas] = useState([]);
     const{me} = useUsers();
 
-    useEffect(()=>{
-        getAllUserEvasForThisUser();
-    },[userEvas])
 
     const getAllUserEvasForThisUser = () =>{
         axios.get("/api/userseva/"+me)
@@ -26,6 +23,8 @@ export default function EvaUserArea(props: EvaUserProps){
             .then((userEvas)=> setUserEvas(userEvas))
             .catch(error => {console.log(error)})
     }
+
+    getAllUserEvasForThisUser()
 
     return(
         <main>
